7138e3907bd8fe4e8dee24fef48b78ae5acaa0b4
salmon <iamfhaa@gmail.com>
2022-05-03 04:05:47 +0700

    asdas
    dasd

--- TestingSQL/saveEvent.sql
+++ TestingSQL/saveEvent.sql
@@ -1,4 +1,5 @@
 use scheduledb;
+set names utf8;
 INSERT INTO event values (1,'Somchai Jaidee (OR-7)','somchai.jai@mail.kmutt.com','DevOps/Infra Clinic','2022-05-23:13:30:00',30,null,2);
 INSERT INTO event values (2,'Somsri Rakdee (SJ-3)','somsri.rak@mail.kmutt.com','Project Management Clinic','2022-04-27:09:30:00',30,'ขอปรึกษาปัญหาเพื่อนไม่ช่วยงาน',1);
 INSERT INTO event values (3,'สมเกียรติ ขยันเรียน กลุ่ม TT-4','somkiat.kay@mail.kmutt.com','Database Clinic','2022-05-23:16:30:00',30,null,3);
